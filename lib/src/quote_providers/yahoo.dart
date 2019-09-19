957aef49c39a2aeccde8281fcf90580d11f34eb7
argos <levantedevelopments@gmail.com>
2019-09-19 12:05:59 +0200

    refactor yahoo

--- lib/src/quote_providers/yahoo.dart
+++ lib/src/quote_providers/yahoo.dart
@@ -7,44 +7,76 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
 
+class YahooApiException implements Exception {
+  final int statusCode;
+  final String message;
+
+  const YahooApiException({this.statusCode, this.message});
+}
+
 class Yahoo {
   static Future<Map<String, Map<String, dynamic>>> downloadRaw(
       List<String> symbols, http.Client client, Logger logger) async {
-    final Map<String, Map<String, dynamic>> retrieved =
+    final Map<String, Map<String, dynamic>> results =
         <String, Map<String, dynamic>>{};
 
     try {
-      final String symbolList = symbols.join(',');
+      final Map<String, dynamic> quoteRaw = await _getRawQuote(symbols, client);
 
-      final http.Response response = await client.get(
-          'https://query1.finance.yahoo.com/v7/finance/quote?symbols=' +
-              symbolList);
-
-      if (response != null && response.statusCode == 200) {
-        final Map<String, dynamic> quoteData = const JsonDecoder()
-            .convert(response.body)['quoteResponse'] as Map<String, dynamic>;
-
-        // Search in the answer obtained the data corresponding to the symbols.
-        // If requested symbol data is found add it to [portfolioQuotePrices].
-        for (String symbol in symbols) {
-          for (dynamic marketData in quoteData['result']) {
-            if (marketData['symbol'] == symbol) {
-              // ignore: avoid_as
-              retrieved[symbol] = marketData as Map<String, dynamic>;
-            }
+      // Search in the answer obtained the data corresponding to the symbols.
+      // If requested symbol data is found add it to [portfolioQuotePrices].
+      for (String symbol in symbols) {
+        for (dynamic marketData in quoteRaw['result']) {
+          if (marketData['symbol'] == symbol) {
+            // ignore: avoid_as
+            results[symbol] = marketData as Map<String, dynamic>;
           }
         }
-      } else {
-        logger
-            .e('Failed to download yahoo quote data for: ' + symbols.join(','));
       }
-    } catch (e) {
+    } on YahooApiException catch (e) {
       logger.e(
-          'Exception caught while parsing downloaded yahoo quote data for: ' +
-              symbols.join(','));
+          'YahooApiException{symbols: ${symbols.join(',')}, statusCode: ${e.statusCode}, message: ${e.message}}');
     }
 
-    return retrieved;
+    for (String symbol in symbols) {
+      if (!results.containsKey(symbol)) {
+        logger.e('YahooApi: Symbol $symbol not found.');
+      }
+    }
+
+    return results;
+  }
+
+  static Future<Map<String, dynamic>> _getRawQuote(
+      List<String> symbols, http.Client client) async {
+    final String symbolList = symbols.join(',');
+
+    final String quoteUrl =
+        'https://query1.finance.yahoo.com/v7/finance/quote?symbols=' +
+            symbolList;
+    try {
+      final http.Response quoteRes = await client.get(quoteUrl);
+      if (quoteRes != null &&
+          quoteRes.statusCode == 200 &&
+          quoteRes.body != null) {
+        return parseRawQuote(quoteRes.body);
+      } else {
+        throw YahooApiException(
+            statusCode: quoteRes?.statusCode, message: 'Invalid response.');
+      }
+    } on http.ClientException {
+      throw const YahooApiException(message: 'Connection failed.');
+    }
+  }
+
+  static Map<String, dynamic> parseRawQuote(String quoteResBody) {
+    try {
+      return const JsonDecoder().convert(quoteResBody)['quoteResponse']
+          as Map<String, dynamic>;
+    } catch (e) {
+      throw const YahooApiException(
+          statusCode: 200, message: 'Quote was not parseable.');
+    }
   }
 
   static Map<String, String> parsePrice(Map<String, dynamic> rawQuote) {
