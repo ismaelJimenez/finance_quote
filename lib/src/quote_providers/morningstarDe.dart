01da0a52eea09ecfe4903821ad2c272a0f90b8b4
argos <levantedevelopments@gmail.com>
2019-09-19 11:06:54 +0200

    added parallel quote retrieval for Morningstar

--- lib/src/quote_providers/morningstarDe.dart
+++ lib/src/quote_providers/morningstarDe.dart
@@ -8,51 +8,85 @@ import 'package:intl/intl.dart';
 import 'package:html/parser.dart' show parse;
 import 'package:html/dom.dart';
 
+class MorningstarDeApiException implements Exception {
+  final int statusCode;
+  final String message;
+
+  const MorningstarDeApiException({this.statusCode, this.message});
+}
+
 class MorningstarDe {
   static Future<Map<String, Map<String, dynamic>>> downloadRaw(
       List<String> symbols, http.Client client, Logger logger) async {
-// Retrieved market data.
-    final Map<String, Map<String, dynamic>> retrieved =
+    final Map<String, Map<String, dynamic>> results =
         <String, Map<String, dynamic>>{};
 
+    // We are running the fetch of each price in parallel with Future.wait.
+    // Here, we catch FinanceQuoteProviderApiExceptions so that one API exception
+    // doesn't stop the whole fetch.
+    final Iterable<Future<void>> futureQuotes =
+        symbols.map<Future<void>>((String symbol) async {
+      try {
+        final Map<String, dynamic> quoteRaw =
+            await _getRawQuote(symbol, client);
+        if (quoteRaw.isNotEmpty) {
+          results[symbol] = quoteRaw;
+        }
+      } on MorningstarDeApiException catch (e) {
+        print(
+            'FinanceQuote MorningstarDeApiException{symbol: $symbol, statusCode: ${e.statusCode}, message: ${e.message}}');
+      }
+    });
+
+    await Future.wait(futureQuotes);
+
+    return results;
+  }
+
+  static Future<Map<String, dynamic>> _getRawQuote(
+      String symbol, http.Client client) async {
+    final String quoteUrl =
+        'http://tools.morningstar.de/de/stockreport/default.aspx?id=' + symbol;
+    try {
+      final http.Response quoteRes = await client.get(quoteUrl);
+      if (quoteRes != null &&
+          quoteRes.statusCode == 200 &&
+          quoteRes.body != null) {
+        return parseRawQuote(quoteRes.body);
+      } else {
+        throw MorningstarDeApiException(
+            statusCode: quoteRes.statusCode, message: 'Invalid response.');
+      }
+    } on http.ClientException {
+      throw const MorningstarDeApiException(message: 'Connection failed.');
+    }
+  }
+
+  static Map<String, String> parseRawQuote(String quoteResBody) {
     final NumberFormat f = NumberFormat.decimalPattern('de_DE');
 
-    for (String symbol in symbols) {
-      try {
-        final http.Response response = await client.get(
-            'http://tools.morningstar.de/de/stockreport/default.aspx?id=' +
-                symbol);
+    try {
+      final Document document = parse(quoteResBody);
 
-        if (response != null && response.statusCode == 200) {
-          final Document document = parse(response.body);
+      final double price = f
+          .parse((document.querySelector('.price').nodes[0] as Text).data)
+          .toDouble();
 
-          final double price = f
-              .parse((document.querySelector('.price').nodes[0] as Text).data)
-              .toDouble();
+      final String currency =
+          (document.querySelector('.priceInformation').nodes[4] as Text)
+              .data
+              .split(' | ')[1]
+              .split('\n')[0]
+              .trim();
 
-          final String currency =
-              (document.querySelector('.priceInformation').nodes[4] as Text)
-                  .data
-                  .split(' | ')[1]
-                  .split('\n')[0]
-                  .trim();
-
-          retrieved[symbol] = <String, String>{
-            'price': price.toStringAsFixed(2),
-            'currency': currency.toUpperCase(),
-          };
-        } else {
-          logger
-              .e('Failed to download morningstarDe quote data for: ' + symbol);
-        }
-      } catch (e) {
-        logger.e(
-            'Exception caught while parsing downloaded morningstarDe quote data for: ' +
-                symbol);
-      }
+      return <String, String>{
+        'price': price.toStringAsFixed(2),
+        'currency': currency.toUpperCase(),
+      };
+    } catch (e) {
+      throw const MorningstarDeApiException(
+          statusCode: 200, message: 'Quote was not parseable.');
     }
-
-    return retrieved;
   }
 
   static Map<String, String> parsePrice(Map<String, dynamic> rawQuote) {
